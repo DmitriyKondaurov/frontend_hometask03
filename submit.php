0f56b69626cfa9b3d921a34837ef4c358715cbaf
Dmitry Chabanenko <chabanenk0>
2015-02-23 13:20:24 +0200

    initial commit

---
+++ submit.php
@@ -0,0 +1,33 @@
+<?php
+
+
+/*
+array (size=18)
+  'firstname' => string 'sfsdfds' (length=7)
+  'secondname' => string 'sdfs' (length=4)
+  'gender' => string 'm' (length=1)
+  'age' => string '45' (length=2)
+  'gender2' => string 'f' (length=1)
+  'birthday' => string '1984-01-19' (length=10)
+  'status' => string 'free' (length=4)
+  'status-soc' => string 'busy' (length=4)
+  'address' => string 'home' (length=4)
+  'activities' => 
+    array (size=4)
+      'sleeping' => string 'on' (length=2)
+      'friends' => string 'on' (length=2)
+      'fishing' => string 'on' (length=2)
+      'games' => string 'on' (length=2)
+  'frequency' => string '3' (length=1)
+  'books' => string '9999' (length=4)
+  'comments' => string '    some comments' (length=17)
+  'multiplesel' => string '3' (length=1)
+  'samplefield' => string 'dfsdf' (length=5)
+  'email' => string 'chedreamtester@gmail.com' (length=24)
+  'spam' => 
+    array (size=1)
+      'equipment' => string 'on' (length=2)
+  'complexity' => string 'easy' (length=4)
+
+*/
+var_dump($_REQUEST);
